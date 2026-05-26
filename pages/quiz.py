62412cf8a8bb869888fb628bc08f172c0c4ddeb4
lucasgabriel <lucasgabriel30015@gmail.com>
2026-05-26 19:01:51 -0300

    Correção do quiz

--- pages/quiz.py
+++ pages/quiz.py
@@ -16,13 +16,19 @@ def tela_quiz():
         for opcao in pergunta['opcoes']:
             print(opcao)
 
-        resposta_usuario = input('Digite a letra da resposta correta: ').strip().upper()
+        resposta_usuario = input('Digite a letra da resposta correta ou digite "MENU" para voltar ao menu principal: ').strip().upper()
 
-        if resposta_usuario == pergunta['resposta']:
+        if resposta_usuario == 'MENU':
+            limpar_tela()
+            return
+        elif resposta_usuario == pergunta['resposta']:
             print('✅ Resposta correta!')
             pontuacao += 5
         else:
             print(f'❌ Resposta incorreta! A resposta correta é: {pergunta["resposta"]}')
+        
+        input('Pressione Enter para continuar para a próxima pergunta...')
+        limpar_tela()
 
     print(f'PARABÉNS!🎉 Sua pontuação final é: {pontuacao}')
     input('Pressione Enter para voltar ao menu principal:')
