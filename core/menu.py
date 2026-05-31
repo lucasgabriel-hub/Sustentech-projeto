dc3cd052b0479636e5b4350ba3c63a8bf5a4dc4b
lucasgabriel <lucasgabriel30015@gmail.com>
2026-05-31 18:28:12 -0300

    função mapa

--- core/menu.py
+++ core/menu.py
@@ -7,6 +7,8 @@ from pages.usuarios import tela_usuarios
 from database.connection import conectar_banco
 from pages.quiz import tela_quiz
 from utils.utilidades import limpar_tela
+from pages.loja import tela_loja
+from pages.rotas import rotas_descarte
 
 def menu_principal(conexao, usuario):
     usuario_id = usuario["id"]
@@ -22,7 +24,9 @@ def menu_principal(conexao, usuario):
         print('3. Quiz')
         print('4. Recomendações')
         print('5. Usuários')
-        print('6. Sair')
+        print('6. Loja')
+        print('7. Rotas de Descarte')
+        print('8. Sair')
         print("-"*40)
 
         feature_escolhida = input('Digite um número correspondente à opção: ')
@@ -43,6 +47,12 @@ def menu_principal(conexao, usuario):
             limpar_tela()
             tela_usuarios(conexao, usuario_id)
         elif feature_escolhida == '6':
+            limpar_tela()
+            tela_loja()
+        elif feature_escolhida == '7':
+            limpar_tela()
+            rotas_descarte()
+        elif feature_escolhida == '8':
             print('Saindo do programa...')
             conexao.close()
             break
---
+++ pages/rotas.py
@@ -0,0 +1,4 @@
+def rotas_descarte():
+    print("="*40)
+    print('Rotas de Descarte')
+    print("="*40)
